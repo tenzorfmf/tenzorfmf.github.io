a3f06ee69b53a009ee2b616a0cd919510ddee4ba
tenzorji <tenzorfmf@proton.me>
2024-08-29 13:29:00 +0200

    skript

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function toggleDropdown(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === "none" || element.style.display === "") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
